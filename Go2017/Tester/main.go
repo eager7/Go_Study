7ab30ed9eed1b0a55509a76dc6e2180388623a54
eager7 <panchagntao@gmail.com>
2018-04-28 15:47:25 +0800

    1. 修改出错的库文件

--- Go2017/Tester/main.go
+++ Go2017/Tester/main.go
@@ -6,9 +6,14 @@ import (
 	"sync"
 	"time"
 	"github.com/garyburd/redigo/redis"
+	"github.com/eager7/go/mlog"
 )
 
 func main(){
+mlog.Debug.Println("test")
+}
+
+func testSql(){
 	c, err := redis.Dial("tcp", "127.0.0.1:6379")
 	if err != nil{
 		fmt.Println("Connect to redis error:", err.Error())
--- Go2017/rpc_database/remote/method.go
+++ Go2017/rpc_database/remote/method.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"context"
 	"github.com/pkg/errors"
-	"github.com/eager7/go/mlog"
 )
 
 type Args struct {
@@ -24,7 +23,6 @@ func NewMethod()(*Method){
 }
 
 func (m *Method)Add(ctx context.Context, key, value string)(reply int32, err error){
-	mlog.Debug.Println("Add Method key:", key, "value:", value)
 	err = m.s.Set(key, value)
 	if err != nil {
 		fmt.Println(err)
@@ -37,7 +35,6 @@ func (m *Method)Add(ctx context.Context, key, value string)(reply int32, err err
 }
 
 func (m *Method)Get(ctx context.Context, key string)(value string, err error){
-	mlog.Debug.Println("Get Method:", key)
 	v, err := m.s.Get(key)
 	if err != nil {
 		fmt.Println(err)
