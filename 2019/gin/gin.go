73e0d1cd9895afec31f7c4726ea2cc818adda9aa
eager7 <panchangtao@gmail.com>
2019-02-19 20:26:09 +0800

    添加汇率转换代码

--- 2019/gin/gin.go
+++ 2019/gin/gin.go
@@ -1,8 +1,14 @@
 package gin
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"time"
 )
 
 type BaseResp struct {
@@ -46,4 +52,69 @@ func PingHandle(context *gin.Context) {
 		context.JSON(200, resp)
 	}
 	context.JSON(200, gin.H{"message": fmt.Sprintf("%s pong", who.who)})
+}
+
+func SendAndRecv(reqUrl string, recvBody interface{}) error {
+	body, err := SendRequest("GET", nil, reqUrl)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(body, &recvBody); err != nil {
+		fmt.Println("Unmarshal err:", reqUrl, " body:", string(body))
+		return err
+	}
+	return nil
+}
+
+func SendRequest(method string, tReq interface{}, reqUrl string) (body []byte, err error) {
+	Client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   10 * time.Second,
+				KeepAlive: 10 * time.Second,
+			}).DialContext,
+			MaxIdleConns:        3,
+			MaxIdleConnsPerHost: 3,
+			IdleConnTimeout:     time.Duration(10) * time.Second,
+		},
+		Timeout: time.Duration(300) * time.Second,
+	}
+	//通常是采用strings.NewReader函数，将一个string类型转化为io.Reader类型，或者bytes.NewBuffer函数，将[]byte类型转化为io.Reader类型。
+	reqBuffers, _ := json.Marshal(tReq)
+	reqByte := bytes.NewBuffer(reqBuffers)
+
+	req, err := http.NewRequest("GET", "http://baidu.com", nil)
+	if tReq == nil {
+		req, err = http.NewRequest(method, reqUrl, nil)
+	} else {
+		req, err = http.NewRequest(method, reqUrl, reqByte)
+	}
+	if err != nil {
+		fmt.Println("SendRequest  failed.  req_buf:", string(reqBuffers), "req_url:", reqUrl, " err:", err)
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := Client.Do(req)
+	if err != nil && resp == nil {
+		fmt.Println("SendRequest  failed.  req_buf:", string(reqBuffers), " req_url:", reqUrl, " err:", err)
+		return nil, err
+	}
+	defer checkError(resp.Body.Close)
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("SendRequest  failed.  req_buf:", string(reqBuffers), "req_url:", reqUrl, " err:", err)
+		return nil, err
+	}
+	return body, nil
+}
+
+func checkError(callBacks ...func() error) {
+	for _, callBack := range callBacks {
+		if err := callBack(); err != nil {
+			fmt.Println(err)
+		}
+	}
 }
--- 2019/gin/gin_test.go
+++ 2019/gin/gin_test.go
@@ -5,3 +5,17 @@ import "testing"
 func TestInitializeGin(t *testing.T) {
 	InitializeGin()
 }
+
+type Resp struct {
+	value     float64
+	text      string
+	timestamp int64
+}
+
+func TestSendRequest(t *testing.T) {
+	resp := Resp{}
+	if err := SendAndRecv("https://forex.1forge.com/1.0.3/convert?from=USD&to=CNH&quantity=1&api_key=irSQh8AuW0x0Cu6JSbd8VkTGLBOoCdkn", &resp); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(resp)
+}
