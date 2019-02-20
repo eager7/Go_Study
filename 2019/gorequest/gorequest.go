c6f2c77d8b735504872fb9c165a929229bad98c9
eager7 <panchangtao@gmail.com>
2019-02-20 20:52:57 +0800

    go request

---
+++ 2019/gorequest/gorequest.go
@@ -0,0 +1,20 @@
+package gorequest
+
+import (
+	"fmt"
+	"github.com/parnurzeal/gorequest"
+)
+
+func Initialize() {
+	resp, body, errs := gorequest.New().Get("https://forex.1forge.com/1.0.3/convert?from=USD&to=CNH&quantity=1&api_key=irSQh8AuW0x0Cu6JSbd8VkTGLBOoCdkn").End()
+	for _, err := range errs {
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println(resp, body)
+}
+
+
+
+
---
+++ 2019/gorequest/gorequest_test.go
@@ -0,0 +1,7 @@
+package gorequest
+
+import "testing"
+
+func TestInitialize(t *testing.T) {
+	Initialize()
+}
