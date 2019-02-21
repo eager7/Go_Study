ada6c1f9aa62851da1b14d2000aee980405d13fe
eager7 <panchangtao@gmail.com>
2019-02-21 10:58:02 +0800

    go request

--- 2019/gorequest/gorequest.go
+++ 2019/gorequest/gorequest.go
@@ -3,10 +3,11 @@ package gorequest
 import (
 	"fmt"
 	"github.com/parnurzeal/gorequest"
+	"time"
 )
 
 func Initialize() {
-	resp, body, errs := gorequest.New().Get("https://forex.1forge.com/1.0.3/convert?from=USD&to=CNH&quantity=1&api_key=irSQh8AuW0x0Cu6JSbd8VkTGLBOoCdkn").End()
+	resp, body, errs := gorequest.New().Timeout(time.Second*5).Get("https://forex.1forge.com/1.0.3/convert?from=USD&to=CNH&quantity=1&api_key=irSQh8AuW0x0Cu6JSbd8VkTGLBOoCdkn").End()
 	for _, err := range errs {
 		if err != nil {
 			panic(err)
