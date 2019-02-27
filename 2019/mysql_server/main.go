5ffab6ef7a9f014722493038fd0877b80c8fe57c
eager7 <panchangtao@gmail.com>
2019-02-27 16:31:22 +0800

    Time

--- 2019/mysql_server/main.go
+++ 2019/mysql_server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"database/sql/driver"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"strings"
 	"time"
 )
 
@@ -13,6 +15,7 @@ type logger struct{}
 func (logger) Print(v ...interface{}) {
 	fmt.Println(v)
 }
+
 func (EOSTokenPriceInfo) TableName() string {
 	return "t_token_price_info"
 }
@@ -24,7 +27,7 @@ type EOSTokenPriceInfo struct {
 	EOSPrice     float64
 	USDPrice     float64
 	Source       string
-	Timestamp    string
+	Timestamp    BlockTime
 }
 
 type EosTokenGroup struct {
@@ -45,25 +48,22 @@ func main() {
 	db := InitializeGorm()
 	defer checkError(db.sql.Close, db.gorm.Close)
 
-	SearchAll(db)
-
-	if false {
+	if true {
 		if err := SearchList(db, []TokenRequest{{
 			symbol:  "SVN",
 			account: "eoseventoken",
-		}, {
-			symbol:  "POKER",
-			account: "eospokercoin",
-		}}...); err != nil {
+		}, }...); err != nil {
 			panic(err)
 		}
+	} else {
+		SearchAll(db)
 	}
 
 }
 
 func InitializeGorm() *mysql {
-	dataSourceName := "root:zJY121123!@tcp(127.0.0.1:3305)/" +
-		"eos_park_canada_2?charset=utf8mb4&parseTime=true&loc=Local"
+	dataSourceName := "root:zJY121123!@tcp(127.0.0.1:3306)/" +
+		"eos_park?charset=utf8mb4&parseTime=true&loc=Local&parseTime=true"
 	MySQLInlineActionClient, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
 		panic(fmt.Sprintf("sql.Open command failed. err:%s, dataSourceName: %s", err.Error(), dataSourceName))
@@ -84,7 +84,7 @@ func InitializeGorm() *mysql {
 	fmt.Println("connect sql success")
 	return &mysql{
 		sql:  MySQLInlineActionClient,
-		gorm: gormDefaultDB,
+		gorm: gormDefaultDB.LogMode(true),
 	}
 }
 
@@ -92,14 +92,16 @@ func SearchAll(db *mysql) {
 	//var modelTokenPrices []EOSTokenPriceInfo
 	var modelTokenPrices []EOSTokenPriceInfo
 	start := time.Now().UnixNano()
-	if err := db.gorm.Raw("select * from t_token_price_info where (symbol,issue_account,timestamp) in " +
-		"(select symbol, issue_account, max(timestamp) from t_token_price_info group by symbol, issue_account)").Scan(&modelTokenPrices); err != nil {
+	/*if err := db.gorm.Raw("select * from t_token_price_info where (symbol,exec_account,`index`) in " +
+		"(select symbol, exec_account, max(`index`) from t_token_price_info group by symbol, exec_account)").Scan(&modelTokenPrices); err != nil {
 		fmt.Println(err)
-	}
+	}*/
+	//db.gorm.Select()
 
 	end := time.Now().UnixNano()
 	fmt.Println("find result time:", (end-start)/1000000, "ms")
 	fmt.Println(len(modelTokenPrices))
+	//fmt.Println(modelTokenPrices)
 }
 
 type TokenRequest struct {
@@ -113,7 +115,7 @@ func SearchList(db *mysql, tokens ...TokenRequest) error {
 	for _, token := range tokens {
 		fmt.Println("search token:", token)
 		var modelTokenPrice EOSTokenPriceInfo
-		if err := db.gorm.Where("symbol = ? AND issue_account = ?", token.symbol, token.account).
+		if err := db.gorm.Where("symbol = ? AND exec_account = ?", token.symbol, token.account).
 			Order("`index` DESC").Limit(1).Find(&modelTokenPrice).Error; err != nil {
 			return err
 		}
@@ -131,4 +133,61 @@ func checkError(callBacks ...func() error) {
 			fmt.Println(err)
 		}
 	}
+}
+
+type BlockTime struct {
+	time.Time
+}
+const BLOCK_TIME_FORMAT = "2006-01-02T15:04:05.000"
+
+func NewBlockTime(t time.Time) BlockTime {
+	return BlockTime{Time: t}
+}
+
+func (this BlockTime) MarshalJSON() ([]byte, error) {
+	t := this.UTC().Format(BLOCK_TIME_FORMAT)
+	var stamp = fmt.Sprintf("\"%s\"", t)
+	return []byte(stamp), nil
+}
+
+func (this *BlockTime) UnmarshalJSON(b []byte) error {
+	// timeStr := string(b)
+	timeStr := strings.Replace(string(b), "\"", "", -1)
+	d, err := time.Parse(BLOCK_TIME_FORMAT, timeStr)
+	if err != nil {
+		return err
+	}
+
+	*this = BlockTime{Time: d}
+	return nil
+}
+
+func (this BlockTime) Value() (driver.Value, error) {
+	var zeroTime time.Time
+	if this.UnixNano() == zeroTime.UnixNano() {
+		return nil, nil
+	}
+	return this.Time, nil
+}
+
+func (this *BlockTime) Scan(v interface{}) error {
+	value, ok := v.(time.Time)
+	if ok {
+		*this = BlockTime{Time: value}
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to BlockTime\n", v)
+}
+
+func (this BlockTime) ToString() string {
+	return this.Format(BLOCK_TIME_FORMAT)
+}
+
+func (b *BlockTime) FromString(timeStr string) error {
+	t, err := time.Parse(BLOCK_TIME_FORMAT, timeStr)
+	if err != nil {
+		return err
+	}
+	b.Time = t
+	return nil
 }
