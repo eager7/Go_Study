f93858a3579e456444396347a18ab35cb4abd3f2
eager7 <panchangtao@gmail.com>
2019-03-14 17:53:11 +0800

    token list

--- 2019/gorequest/gorequest.go
+++ 2019/gorequest/gorequest.go
@@ -1,6 +1,7 @@
 package gorequest
 
 import (
+	"errors"
 	"fmt"
 	"github.com/parnurzeal/gorequest"
 	"net/http"
@@ -10,7 +11,7 @@ import (
 var url = "https://forex.1forge.com/1.0.3/convert?from=USD&to=CNH&quantity=1&api_key=irSQh8AuW0x0Cu6JSbd8VkTGLBOoCdkn"
 
 func Initialize() {
-	resp, body, errs := gorequest.New().Timeout(time.Second*5).Get(url).End()
+	resp, body, errs := gorequest.New().Timeout(time.Second * 5).Get(url).End()
 	for _, err := range errs {
 		if err != nil {
 			panic(err)
@@ -19,7 +20,7 @@ func Initialize() {
 	fmt.Println(resp, body)
 }
 
-func GetCurrency(){
+func GetCurrency() {
 	resp := struct {
 		Value     float64
 		Text      string
@@ -33,3 +34,53 @@ func GetCurrency(){
 	}
 }
 
+type TokenPrice struct {
+	Symbol      string
+	ExecAccount string
+	EOSPrice    float64
+	USDPrice    float64
+	Source      string
+	Timestamp   time.Time
+}
+
+func FetchTokenPriceListFromNewDex() ([]TokenPrice, error) {
+	type tTokenTicker struct {
+		Symbol   string  `json:"symbol"`
+		Contract string  `json:"contract"`
+		Currency string  `json:"currency"`
+		Last     float64 `json:"last"`
+		Change   float64 `json:"change"`
+		High     float64 `json:"high"`
+		Low      float64 `json:"low"`
+		Amount   float64 `json:"amount"`
+		Volume   float64 `json:"volume"`
+	}
+	type tResponse struct {
+		Code int            `json:"code"`
+		Data []tTokenTicker `json:"data"`
+	}
+
+	fmt.Println("begin get token list...", time.Now().UTC())
+	var response tResponse
+	resp, _, errs := gorequest.New().Get("https://api.newdex.io/v1/ticker/all").EndStruct(&response)
+	if errs != nil {
+		return nil, errs[0]
+	}
+	fmt.Println("end get token list...", time.Now().UTC())
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("fail to fetch token price from newdex. status: " + resp.Status)
+	}
+
+	now := time.Now()
+	priceList := make([]TokenPrice, len(response.Data))
+	for i, ticker := range response.Data {
+		priceList[i] = TokenPrice{
+			Symbol:      ticker.Currency,
+			ExecAccount: ticker.Contract,
+			EOSPrice:    ticker.Last,
+			Source:      "newdex",
+			Timestamp:   now,
+		}
+	}
+	return priceList, nil
+}
--- 2019/gorequest/gorequest_test.go
+++ 2019/gorequest/gorequest_test.go
@@ -5,3 +5,11 @@ import "testing"
 func TestInitialize(t *testing.T) {
 	GetCurrency()
 }
+
+func TestFetchTokenPriceListFromNewDex(t *testing.T) {
+	if list, err := FetchTokenPriceListFromNewDex(); err != nil {
+		t.Fatal(err)
+	} else {
+		t.Log(list)
+	}
+}
