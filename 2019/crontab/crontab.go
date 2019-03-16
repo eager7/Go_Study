d6e8c7a73151d1ebbba3d5287bf74564110ca122
eager7 <panchangtao@gmail.com>
2019-03-16 19:43:05 +0800

    crontab

--- 2019/crontab/crontab.go
+++ 2019/crontab/crontab.go
@@ -10,22 +10,40 @@ func Initialize() error {
 	task := cron.New()
 	//秒里有1时执行任务
 	if err := task.AddFunc("1 * * * *", func() {
-		fmt.Println("task 1:", time.Now().Local())
+		//fmt.Println("task 1 for every minute:", time.Now().Local())
 	}); err != nil {
 		return err
 	}
 	//秒里有3或者15时执行任务
 	if err := task.AddFunc("3,15 * * * *", func() {
-		fmt.Println("task 2:", time.Now().Local())
+		//fmt.Println("task 2 for 3,15 every minute:", time.Now().Local())
 	}); err != nil {
 		return err
 	}
 	//每秒执行一次
 	if err := task.AddFunc("@every 1s", func() {
-		fmt.Println("task 3:", time.Now().Local())
+		//fmt.Println("task 3:", time.Now().Local())
 	}); err != nil {
 		return err
 	}
+
+	if err := task.AddFunc("0 */2 * * *", func() {
+		//fmt.Println("task 4 for 2 minute:", time.Now().Local())
+	}); err != nil {
+		return err
+	}
+
+	if err := task.AddFunc("@every 1h", func() {
+		fmt.Println("task 5 for 1 hour:", time.Now().Local())
+	}); err != nil {
+		return err
+	}
+	if err := task.AddFunc("0 0 */2 * *", func() {
+		fmt.Println("task 6 for 1.5 hour:", time.Now().Local())
+	}); err != nil {
+		return err
+	}
+
 	task.Start()
 	fmt.Println(task.Entries())
 	return nil
--- 2019/crontab/crontab_test.go
+++ 2019/crontab/crontab_test.go
@@ -2,12 +2,12 @@ package crontab
 
 import (
 	"testing"
-	"time"
 )
 
 func TestInitialize(t *testing.T) {
 	if err := Initialize(); err != nil {
 		t.Fatal(err)
 	}
-	time.Sleep(time.Second*100)
+	make(chan struct{})<- struct{}{}
+
 }
