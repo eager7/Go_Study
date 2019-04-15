670df9edde4ac84e51b3e36fd6fec0580ebaf796
eager7 <panchangtao@gmail.com>
2019-04-15 20:27:55 +0800

    mcache

---
+++ 2019/mcache/mcache_test.go
@@ -0,0 +1,27 @@
+package mcache
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"testing"
+)
+
+func TestMCache(t *testing.T) {
+	router := gin.Default()
+	router.GET("/:who/ping", PingHandle)
+	if err := router.Run(); err != nil {
+		panic(err)
+	}
+}
+
+type Ping struct {
+	who string
+}
+
+func PingHandle(context *gin.Context) {
+	who := Ping{who: context.Param("who")}
+	if context.BindQuery(&who) != nil {
+		context.JSON(200, fmt.Sprintf("you failed, %s", who))
+	}
+	context.JSON(200, gin.H{"message": fmt.Sprintf("hi, %s", who.who)})
+}
