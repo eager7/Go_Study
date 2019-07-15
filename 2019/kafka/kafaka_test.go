366da3c117f7d8d33a6250279165097d96c17b6d
eager7 <panchangtao@gmail.com>
2019-07-15 15:49:42 +0800

    test

--- 2019/kafka/kafaka_test.go
+++ 2019/kafka/kafaka_test.go
@@ -3,8 +3,8 @@ package kafka
 import "testing"
 
 func TestInitialize(t *testing.T) {
-	addresses := []string{`47.52.147.232:9092`, `47.244.47.241:9092`, `47.75.179.251:9092`}
-	topic := []string{`eos_blockchain_transaction`, `eos_blockchain_block`}
-	group := `eos_blockchain`
+	addresses := []string{`192.168.0.1:9092`}
+	topic := []string{`test1`, `test2`}
+	group := `test`
 	Initialize(addresses, group, topic)
 }
