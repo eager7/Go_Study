4e6434542ec8c859a653501a754194248bd6889f
eager7 <panchangtao@gmail.com>
2019-07-18 17:39:13 +0800

    d

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
-import _"github.com/eager7/go_study/2019/kafka"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("main")
+	var c chan int
+	go func() {
+		c <- 1
+		fmt.Println("done")
+	}()
+	time.Sleep(time.Second * 5)
 }
