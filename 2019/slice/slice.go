9b277e6555d4df01393c882e1cfe79a73bf81055
plainchant <plainchant@plainchant-desktop.lan>
2019-08-09 19:43:54 +0800

    增加slice测试函数

--- 2019/slice/slice.go
+++ 2019/slice/slice.go
@@ -1,5 +1,9 @@
 package slice
 
+import (
+	"fmt"
+)
+
 func SplitSlice(slice []int, n int) [][]int {
 	var sTemp [][]int
 	s := len(slice) / n
@@ -10,6 +14,7 @@ func SplitSlice(slice []int, n int) [][]int {
 	sTemp = append(sTemp, slice[s*n:])
 	return sTemp
 }
+
 func SplitSliceLen(slice []int, length int) [][]int {
 	var sTemp [][]int
 	n := len(slice) / length
@@ -19,4 +24,44 @@ func SplitSliceLen(slice []int, length int) [][]int {
 	}
 	sTemp = append(sTemp, slice[length*n:])
 	return sTemp
+}
+
+
+func AppendSlice() {
+	type m struct {
+		s []interface{}
+	}
+	a := m{
+		s: make([]interface{}, 0, 0),
+	}
+	a.s = append(a.s, "a")
+	a.s = append(a.s, "b")
+	a.s = append(a.s, "c")
+
+	b := a
+	a.s = append(a.s, "d")
+	a.s = append(a.s, "e")
+
+	b.s = append(b.s, "1")
+	b.s = append(b.s, "2")
+	b.s = append(b.s, "3")
+
+	fmt.Println(1111, a, b)
+}
+
+
+func PSlice() {
+	a := make([]string, 0, 8)
+	a = append(a, "a", "b", "c")
+
+	b := a
+	a = append(a, "d")
+	a = append(a, "e")
+
+	fmt.Println(a, b)
+
+	b = append(b, "1")
+	b = append(b, "2")
+	b = append(b, "3")
+	fmt.Println(a, b)
 }
